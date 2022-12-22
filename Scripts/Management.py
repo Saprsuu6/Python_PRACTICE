e939bc7a054e49b237e913afe2c06178e67219d2
Andry Saprigin <coffeei.2002@gamil.com>
2022-12-22 22:13:50 +0300

    commit

--- Scripts/Management.py
+++ Scripts/Management.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class MyInfo:
     def __init__(self) -> None:
         self.info = []
@@ -8,16 +11,23 @@ class MyInfo:
 
         self.info.append(info)
 
-    def get_info(self) -> dict or str:
+    def get_info(self) -> list or str:
         return self.info if len(self.info) != 0 else 'Infos is empty'
+
+
+def fill_info(my_info: MyInfo) -> None:
+    my_info.add_info({'Name': 'Артемов В.П.', 'Sex': 'Ч',
+                      'BD': datetime.date(1992, 12, 11), 'Country': 'Німеччина', 'Avia_company': 'Lufthansa', 'Departure_date': datetime.date(2022, 2, 12), 'Departure_time': datetime.time(16, 20)})
+    my_info.add_info({'Name': 'Крейд С.Р.', 'Sex': 'Ч',
+                      'BD': datetime.date(2015, 11, 12), 'Country': 'Ізраїль', 'МАУ': 'Lufthansa', 'Departure_date': datetime.date(2022, 10, 13), 'Departure_time': datetime.time(22, 30)})
 
 
 def main():
     my_info = MyInfo()
 
-    my_info.add_info({"Артемов В.П.", {'Ч', ''}})
-
+    fill_info(my_info)
     print(my_info.get_info())
+    # print(my_info.get_info()[0]['BD'])
 
 
 if __name__ == "__main__":
