653723c2000fc39312f28df0545ab6d539c46d21
Andry Saprigin <coffeei.2002@gamil.com>
2022-12-22 22:31:15 +0300

    commit

--- Scripts/Management.py
+++ Scripts/Management.py
@@ -19,11 +19,15 @@ def fill_info(my_info: MyInfo) -> None:
     my_info.add_info({'Name': 'Артемов В.П.', 'Sex': 'ч',
                       'BD': datetime.date(1992, 12, 11), 'Country': 'Німеччина', 'Avia_company': 'Lufthansa', 'Departure_date': datetime.date(2022, 2, 12), 'Departure_time': datetime.time(16, 20), 'Class': 'Економ', 'Price': 340})
     my_info.add_info({'Name': 'Крейд С.Р.', 'Sex': 'ч',
-                      'BD': datetime.date(2015, 11, 12), 'Country': 'Ізраїль', 'МАУ': 'Lufthansa', 'Departure_date': datetime.date(2022, 10, 13), 'Departure_time': datetime.time(22, 30), 'Class': 'Бізнес', 'Price': 470})
+                      'BD': datetime.date(2015, 11, 12), 'Country': 'Ізраїль', 'Avia_company': 'МАУ', 'Departure_date': datetime.date(2022, 10, 13), 'Departure_time': datetime.time(22, 30), 'Class': 'Бізнес', 'Price': 470})
     my_info.add_info({'Name': 'Сидорова Е.А', 'Sex': 'ж',
-                      'BD': datetime.date(2012, 11, 19), 'Country': 'Іспанія', 'МАУ': 'Lufthansa', 'Departure_date': datetime.date(2022, 9, 22), 'Departure_time': datetime.time(18, 40), 'Class': 'Бізнес', 'Price': 490})
+                      'BD': datetime.date(2012, 11, 19), 'Country': 'Іспанія', 'Avia_company': 'МАУ', 'Departure_date': datetime.date(2022, 9, 22), 'Departure_time': datetime.time(18, 40), 'Class': 'Бізнес', 'Price': 490})
     my_info.add_info({'Name': 'Богданов А.С', 'Sex': 'ч',
-                      'BD': datetime.date(1979, 1, 19), 'Country': 'Іспанія', 'МАУ': 'Lufthansa', 'Departure_date': datetime.date(2021, 12, 30), 'Departure_time': datetime.time(18, 40), 'Class': 'Бізнес', 'Price': 490})
+                      'BD': datetime.date(1979, 1, 19), 'Country': 'Іспанія', 'Avia_company': 'МАУ', 'Departure_date': datetime.date(2021, 12, 30), 'Departure_time': datetime.time(18, 40), 'Class': 'Бізнес', 'Price': 490})
+    my_info.add_info({'Name': 'Буртик Н.К', 'Sex': 'ж',
+                      'BD': datetime.date(1980, 1, 21), 'Country': 'Туреччина', 'Avia_company': 'Turkish Airlines', 'Departure_date': datetime.date(2021, 10, 6), 'Departure_time': datetime.time(7, 30), 'Class': 'Перший', 'Price': 440})
+    my_info.add_info({'Name': 'Горкун В.Л.', 'Sex': 'ч',
+                      'BD': datetime.date(2016, 12, 13), 'Country': 'Іспанія', 'Avia_company': 'МАУ', 'Departure_date': datetime.date(2020, 11, 19), 'Departure_time': datetime.time(18, 40), 'Class': 'Бізнес', 'Price': 490})
 
 
 def main():
